3907ef313d4be6eb7b322670d4e4c293b316f7aa
Mugdha Sonawane <mugdha.sonawane@globant.com>
2018-03-27 12:33:55 +0530

    Added routing changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+
     };
   }
 
@@ -19,11 +19,57 @@ class Dashboard extends Component {
   render() {
 
     return (
-      <div className="col-md-12 demo-div heading-section">
-        <div className="lft col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <Stocktable/>
+      <div className="col-md-12 col-lg-12 demo-div heading-section">
+        <div className="row">
+          <div className="header-left col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div>
+              <Stocktable />
+            </div>
+          </div>
+          <div className="header-right col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+            <a href="/" className="logo">
+              <img src="kite-logo.svg" alt="Kite logo" />
+            </a>
+            <div className="app-nav">
+              <a href="/chart/NSE/BANDHANBNK/dashboard" className="">
+                <span>Dashboard</span>
+              </a>
+              <a href="/chart/NSE/BANDHANBNK/orders" className="">
+                <span>Orders</span>
+              </a>
+              <a href="/chart/NSE/BANDHANBNK/holdings" className="">
+                <span>Holdings</span>
+              </a>
+              <a href="/chart/NSE/BANDHANBNK/margins" className="">
+                <span>Funds</span>
+              </a>
+              <a href="/chart/NSE/BANDHANBNK/apps" className="">
+                <span>Apps</span>
+              </a>
+            </div>
+            <div className="right-nav">
+              {/* <div className="notifications-menu">
+                <span id="notifications" className="notifications">
+                  <a className="notifications-widget">
+                  <span class="glyphicon glyphicon-bell"></span>
+                  </a>
+                </span>
+              </div> */}
+              <div className="user-nav">               
+                  <span>YK2729</span>               
+              </div>
+            </div>  
+            <Chart/>    
+            {/* <LineChart/>      */}
+          </div>
         </div>
-        <div className="lft col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+
+
+
+
+        {/* <div className="lft col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <UserInfo/>
         </div>       
         <div className="lft col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -31,9 +77,9 @@ class Dashboard extends Component {
         </div>
         <div className="lft col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <Chart/>
-        </div>
-        
-        
+        </div> */}
+
+
 
       </div>
     );
